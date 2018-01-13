f0218367c1038124f5ec7f4b2e8aabb80e808113
Aliya <aasken01@seattlecentral.edu>
2018-01-13 02:16:07 -0800

    Add files via upload

---
+++ Controllers/MailingController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstLook.Models;
+
+namespace FirstLook.Controllers
+{
+    public class MailingController : Controller
+    {
+        // GET: Mailing
+        public ActionResult Index()
+        {
+            Mailing m1 = new Mailing();
+            m1.FirstName = "Anderson";
+            m1.LastName = "Cooper";
+            m1.Email = "coopy@gmail.com";
+
+            Mailing m2 = new Mailing();
+            m2.FirstName = "Chris";
+            m2.LastName = "Cuomo";
+            m2.Email = "gunshow@gmail.com";
+
+            Mailing m3 = new Mailing();
+            m3.FirstName = "Don";
+            m3.LastName = "Lemon";
+            m3.Email = "bae@gmail.com";
+
+            List<Mailing> mailings = new List<Mailing>();
+            mailings.Add(m1);
+            mailings.Add(m2);
+            mailings.Add(m3);
+
+            return View(mailings);
+        }
+    }
+}
